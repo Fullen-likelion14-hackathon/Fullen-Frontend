679fe248b093c92fad7fd5ac00cadaba6aa9aab7
seungmin <ssmmkim1025@naver.com>
2026-08-05 11:56:09 +0900

    🚚 Rename: 푸터 네비게이션 아이콘 파일명 변경

--- src/components/footerNavigation/FooterNavigation.tsx
+++ src/components/footerNavigation/FooterNavigation.tsx
@@ -1,3 +1,14 @@
+import { NavLink, useLocation } from "react-router-dom";
+
+import mcomActiveIcon from "@/assets/icons/navigation/mcom-active.png";
+import mcomInactiveIcon from "@/assets/icons/navigation/mcom-inactive.png";
+
+import passportActiveIcon from "@/assets/icons/navigation/passport-active.png";
+import passportInactiveIcon from "@/assets/icons/navigation/passport-inactive.png";
+
+import customActiveIcon from "@/assets/icons/navigation/custom-active.png";
+import customInactiveIcon from "@/assets/icons/navigation/custom-inactive.png";
+
 export default function FooterNavigation() {
   return <nav>하단 메뉴</nav>;
 }
