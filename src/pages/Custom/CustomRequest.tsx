f39ddebd8e37d8bb923e023957d7eedb9c2ebcdb
seungmin <ssmmkim1025@naver.com>
2026-08-10 13:18:30 +0900

    feat: 헤더 추가

--- src/pages/Custom/CustomRequest.tsx
+++ src/pages/Custom/CustomRequest.tsx
@@ -1,3 +1,5 @@
+import PageHeader from "@/components/common/PageHeader";
+
 export default function CustomRequest() {
-  return <p>1대1 커스텀 의뢰</p>;
+  return <PageHeader title="1:1 커스텀 신청" backTo="/custom" />;
 }
