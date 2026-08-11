045eee3773058bd349a9fa65e0934cb10a532286
seungmin <ssmmkim1025@naver.com>
2026-08-11 14:13:32 +0900

    🎨 Design: 1:1 주문 메인 페이지 UI 구현

--- src/pages/OneToOneOrder/OneToOneOrderMain.tsx
+++ src/pages/OneToOneOrder/OneToOneOrderMain.tsx
@@ -1,3 +1,48 @@
-export default function OneTeOneOrderMain() {
-  return <p>1대1커스텀 주문하기 메인페이지</p>;
+import floorBg from "@/assets/images/Floor.png";
+import { ProductViewer } from "@/components/custom/viewer/ProductViewer";
+import { Button } from "@base-ui/react/button";
+
+export default function OneToOneOrderMain() {
+  return (
+    <main className="relative mx-auto h-dvh w-full max-w-97.5 overflow-hidden bg-[#F9F4F0]">
+      {/* 배경 */}
+      <img
+        src={floorBg}
+        alt=""
+        className="
+          pointer-events-none
+          absolute
+          left-1/2
+          top-1/2
+          h-auto
+          w-155
+          max-w-none
+          -translate-x-1/2
+          -translate-y-1/2
+          opacity-60
+        "
+      />
+
+      {/* 문구 */}
+      <div className="relative z-10 pt-30 text-center">
+        <p className="text-[20px] font-extrabold text-[#A3642B]">오직 나를 위한 단 하나의 커스텀</p>
+
+        <p className="mt-0.5 text-[16px] font-bold text-[#242D41]">성주재단이 후원하는 작가가</p>
+
+        <p className="text-[20px] font-extrabold text-[#242D41]">
+          당신의 이야기를 직접 그려드립니다.
+        </p>
+      </div>
+
+      {/* 제품 */}
+      <div className="absolute inset-0">
+        <ProductViewer />
+      </div>
+
+      {/* 주문 버튼 */}
+      <Button className="absolute inset-x-6 bottom-50 rounded-xl bg-[#242D41] py-4 text-[20px] font-bold text-white">
+        1:1 커스텀 주문하기
+      </Button>
+    </main>
+  );
 }
