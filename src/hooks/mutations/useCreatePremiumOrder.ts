09ed6aea45fd0136a2e2f60f688aec1bd0b8d878
seungmin <ssmmkim1025@naver.com>
2026-08-19 23:29:41 +0900

    🐛 Fix: 1:1 주문 생성 후 주문 목록 캐시 갱신

--- src/hooks/mutations/useCreatePremiumOrder.ts
+++ src/hooks/mutations/useCreatePremiumOrder.ts
@@ -1,10 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { createPremiumOrder } from "@/api/premiumOrder";
 
 // 1:1 커스텀 주문 생성
 export const useCreatePremiumOrder = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: createPremiumOrder,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["orders"],
+      });
+    },
   });
 };
